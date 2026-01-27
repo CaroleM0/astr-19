c7fff9cc469e41940b764e9df85e73e12f3ef5fe
CaroleM0 <78281824+CaroleM0@users.noreply.github.com>
2026-01-26 18:17:53 -0800

    Journal#1 prompt2 update

---
+++ Prompt2.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+#sum of two floating point numbers
+a = 5.5
+b = 3.4
+sum_result = a+b
+print("Sum:", sum_result)#addition
+print("Type:", type(sum_result))
+
+#difference between two integers
+x = 6
+y = 7
+Diff_result = x-y
+print("Difference:", Diff_result)#subtraction
+print("Type:", type(Diff_result))
+
+#flating pont number x integer number
+m = 4.3
+n = 8
+product_result = m*n
+print("product:", product_result)#multiplication
+print("Type:", type(product_result))
